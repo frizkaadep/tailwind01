d1e8a0184b1de575121fc5acf0123f3d9d96f0ef
Frizka Ade P <106709088+frizkaadep@users.noreply.github.com>
2022-11-19 15:17:27 +0700

    Transform, Transition, Animation
    1. Transform (https://tailwindcss.com/docs/scale)
    2. Transition (https://tailwindcss.com/docs/transition-property)
    3. Animation (https://tailwindcss.com/docs/animation)
    4. Custom Animation & Transition

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,16 @@ module.exports = {
         fap: "#bada55",
         coffe: "#c0ffee",
       },
+      animation: {
+        "spin-slow": "spin 3s linear infinite",
+        goyang: "goyang 1s ease-in-out infinite",
+      },
+      keyframes: {
+        goyang: {
+          "0%, 100%": { transform: "rotate(-3deg)" },
+          "50%": { transform: "rotate(3deg)" },
+        },
+      },
     },
   },
   plugins: [],
